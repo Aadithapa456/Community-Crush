2a4269608d37345af8dbaf1f8c1dff79f96faddb
Aaditya <aadityathapa977@gmail.com>
2025-02-24 21:43:29 +0545

    implement google maps api

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,31 @@
 import React from 'react';
+import {GoogleMap, Marker, useJsApiLoader} from '@react-google-maps/api';
+
+const mapContainerStyle = {
+    width: '100%',
+    height: '100vh',
+};
+
+const center = {
+    lat: 27.7172, // Kathmandu, Nepal
+    lng: 85.3240,
+};
 
 function App() {
+    const {isLoaded} = useJsApiLoader({
+        googleMapsApiKey: import.meta.env.VITE_GOOGLE_API_KEY,
+    })
+    if (!isLoaded) return <div>Loading...</div>;
     return (
-        <div>
-        </div>
+        <>
+            <GoogleMap
+                mapContainerStyle={mapContainerStyle}
+                zoom={8}
+                center={center}
+            >
+                <Marker position={center}/>
+            </GoogleMap>
+        </>
     );
 }
 
