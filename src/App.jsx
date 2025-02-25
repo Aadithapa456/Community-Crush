12998282f4f1619df41aba33bb5e6e9a53fa0aad
Aaditya <aadityathapa977@gmail.com>
2025-02-25 20:56:18 +0545

    feat: implement custom marker

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,72 @@
-import React from 'react';
-import {GoogleMap, Marker, useJsApiLoader} from '@react-google-maps/api';
+// import React from 'react';
+import {GoogleMap, Marker, useJsApiLoader, Circle, InfoWindow} from '@react-google-maps/api';
+import MapForm from "@/components/MapForm.jsx";
+import {useState} from "react";
 
 const mapContainerStyle = {
     width: '100%',
     height: '100vh',
 };
-
 const center = {
     lat: 27.7172, // Kathmandu, Nepal
     lng: 85.3240,
 };
+// const circleOptions = {
+//     strokeColor: '#ffffff',
+//     strokeOpacity: 0.8,
+//     strokeWeight: 2,
+//     fillColor: '#ffffff',
+//     fillOpacity: 0.35,
+//     clickable: false,
+//     draggable: false,
+//     editable: false,
+//     visible: true,
+//     radius: 3000,
+//     center: center,
+// };
 
 function App() {
+    const [mapCoords, setMapCoords] = useState([{lat: 27.7172, lng: 85.3240}, {lat: 27.6588, lng: 85.3247}]);
+    const [viewMarkerInfo, setViewMarkerInfo] = useState(false);
     const {isLoaded} = useJsApiLoader({
         googleMapsApiKey: import.meta.env.VITE_GOOGLE_API_KEY,
     })
     if (!isLoaded) return <div>Loading...</div>;
+    const markerIcon = {
+        url: 'src/assets/pin.png',
+        scaledSize: new window.google.maps.Size(50, 50),
+        origin: new window.google.maps.Point(0, 0),
+        anchor: new window.google.maps.Point(25, 50),
+    }
+    const handleFormSubmit = (data) => {
+        setMapCoords((prevCoords) => [...prevCoords, {lat: parseFloat(data.lat), lng: parseFloat(data.long)}])
+    };
+    console.log(mapCoords);
     return (
-        <>
+        <div className="relative">
             <GoogleMap
                 mapContainerStyle={mapContainerStyle}
                 zoom={8}
-                center={center}
+                center={mapCoords[0]}
             >
-                <Marker position={center}/>
-            </GoogleMap>
-        </>
-    );
+                <Circle options={{center: mapCoords[0]}}/>
+                {mapCoords.map((coord, index) => (
+
+                    <Marker position={coord} icon={markerIcon} key={index} onClick={() => setViewMarkerInfo(true)}>
+                        {viewMarkerInfo &&
+                            <InfoWindow position={coord} onCloseClick={() => setViewMarkerInfo(false)}>
+                            <p>Hello I am here in {coord.lng}</p>
+                    </InfoWindow>}
+            </Marker>
+            ))
+            }
+        </GoogleMap>
+    <div>
+        <MapForm onFormSubmit={handleFormSubmit}/>
+    </div>
+</div>
+)
+    ;
 }
 
 export default App;
---
+++ src/components/MapForm.jsx
@@ -0,0 +1,32 @@
+// import React, {useState} from 'react';
+import {useForm} from "react-hook-form";
+
+function MapForm({onFormSubmit}) {
+    const {register, handleSubmit} = useForm();
+    const onSubmit = (e) => {
+        // e.preventDefault();
+        // console.log("handleSubmit", e);
+        onFormSubmit(e)
+    }
+    return (
+        <form onSubmit={handleSubmit(onSubmit)}
+              className="absolute bottom-4 w-3/4 justify-between left-1/2 transform -translate-x-1/2 flex gap-10 bg-blue-100 shadow-xl p-4 rounded-lg shadow-lg">
+            <div className="form-field">
+                <label htmlFor="lat" className="block text-sm font-semibold text-blue-700 mb-1"></label>
+                <input type="text" id="lat" placeholder="Enter latitude" {...register("lat", {required: true})}
+                       className="py-4 px-2 focus:border-0 border border-gray-200 rounded"/>
+            </div>
+            <div className="form-field">
+                <label htmlFor="long" className="block text-sm font-semibold text-blue-700 mb-1"></label>
+                <input type="text" id="long" placeholder="Enter latitude" {...register("long", {required: true})}
+                       className="py-4 px-2 focus:border-0 border border-gray-200 rounded "/>
+            </div>
+            <button type="submit"
+                    className="bg-blue-600 text-white font-semibold px-6 py-3 rounded-lg hover:bg-blue-700 transition duration-300">Submit
+                now
+            </button>
+        </form>
+    );
+}
+
+export default MapForm;
