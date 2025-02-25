4a2c1dd6969b5971f244ed18879bb75ad6cfaf33
Aaditya <aadityathapa977@gmail.com>
2025-02-25 21:27:40 +0545

    refactor: improve new marker logic

--- src/App.jsx
+++ src/App.jsx
@@ -7,27 +7,10 @@ const mapContainerStyle = {
     width: '100%',
     height: '100vh',
 };
-const center = {
-    lat: 27.7172, // Kathmandu, Nepal
-    lng: 85.3240,
-};
-// const circleOptions = {
-//     strokeColor: '#ffffff',
-//     strokeOpacity: 0.8,
-//     strokeWeight: 2,
-//     fillColor: '#ffffff',
-//     fillOpacity: 0.35,
-//     clickable: false,
-//     draggable: false,
-//     editable: false,
-//     visible: true,
-//     radius: 3000,
-//     center: center,
-// };
 
 function App() {
     const [mapCoords, setMapCoords] = useState([{lat: 27.7172, lng: 85.3240}, {lat: 27.6588, lng: 85.3247}]);
-    const [viewMarkerInfo, setViewMarkerInfo] = useState(false);
+    const [selectedMarker, setSelectedMarker] = useState(null);
     const {isLoaded} = useJsApiLoader({
         googleMapsApiKey: import.meta.env.VITE_GOOGLE_API_KEY,
     })
@@ -41,7 +24,6 @@ function App() {
     const handleFormSubmit = (data) => {
         setMapCoords((prevCoords) => [...prevCoords, {lat: parseFloat(data.lat), lng: parseFloat(data.long)}])
     };
-    console.log(mapCoords);
     return (
         <div className="relative">
             <GoogleMap
@@ -52,21 +34,24 @@ function App() {
                 <Circle options={{center: mapCoords[0]}}/>
                 {mapCoords.map((coord, index) => (
 
-                    <Marker position={coord} icon={markerIcon} key={index} onClick={() => setViewMarkerInfo(true)}>
-                        {viewMarkerInfo &&
-                            <InfoWindow position={coord} onCloseClick={() => setViewMarkerInfo(false)}>
-                            <p>Hello I am here in {coord.lng}</p>
-                    </InfoWindow>}
-            </Marker>
-            ))
-            }
-        </GoogleMap>
-    <div>
-        <MapForm onFormSubmit={handleFormSubmit}/>
-    </div>
-</div>
-)
-    ;
+                    <Marker position={coord} icon={markerIcon} key={index} onClick={() => {
+                        setSelectedMarker(coord);
+                    }}>
+                        {selectedMarker === coord ? (
+                            <InfoWindow position={coord} onCloseClick={() => setSelectedMarker(null)}>
+                                <p>Hello I am here in {coord.lng}</p>
+                            </InfoWindow>
+                        ) : null}
+                    </Marker>
+                ))
+                }
+            </GoogleMap>
+            <div>
+                <MapForm onFormSubmit={handleFormSubmit}/>
+            </div>
+        </div>
+    )
+        ;
 }
 
 export default App;
