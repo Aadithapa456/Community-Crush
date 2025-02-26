f3c6a7ad42fa27aecf18d8cdf4df5898c8551976
Aaditya <aadityathapa977@gmail.com>
2025-02-26 20:51:47 +0545

    feat: implement get direction functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,13 @@
 // import React from 'react';
-import {GoogleMap, Marker, useJsApiLoader, Circle, InfoWindow} from '@react-google-maps/api';
+import {
+    GoogleMap,
+    Marker,
+    useJsApiLoader,
+    Circle,
+    InfoWindow,
+    DirectionsRenderer,
+    DirectionsService
+} from '@react-google-maps/api';
 import MapForm from "@/components/MapForm.jsx";
 import {useState} from "react";
 
@@ -11,6 +19,8 @@ const mapContainerStyle = {
 function App() {
     const [mapCoords, setMapCoords] = useState([{lat: 27.7172, lng: 85.3240}, {lat: 27.6588, lng: 85.3247}]);
     const [selectedMarker, setSelectedMarker] = useState(null);
+    const [direction, setDirection] = useState(null)
+    const [userPosition, setUserPosition] = useState({lat: null, lng: null});
     const {isLoaded} = useJsApiLoader({
         googleMapsApiKey: import.meta.env.VITE_GOOGLE_API_KEY,
     })
@@ -24,6 +34,31 @@ function App() {
     const handleFormSubmit = (data) => {
         setMapCoords((prevCoords) => [...prevCoords, {lat: parseFloat(data.lat), lng: parseFloat(data.long)}])
     };
+    console.log(selectedMarker);
+    const fetchDirections = () => {
+        if (!selectedMarker) {
+            console.log("Please select a marker for directions.");
+            return;
+        }
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition((position) => {
+                const {latitude, longitude} = position.coords;
+                setUserPosition({lat: parseFloat(latitude), lng: parseFloat(longitude)})
+            });
+        }
+        const directionService = new google.maps.DirectionsService();
+        directionService.route({
+            origin: userPosition,
+            destination: selectedMarker,
+            travelMode: google.maps.TravelMode.DRIVING,
+        }, (result, status) => {
+            if (status === google.maps.DirectionsStatus.OK) {
+                setDirection(result);
+            } else {
+                console.log(status);
+            }
+        })
+    }
     return (
         <div className="relative">
             <GoogleMap
@@ -45,9 +80,13 @@ function App() {
                     </Marker>
                 ))
                 }
+
+                {direction && <DirectionsRenderer directions={direction}/>}
             </GoogleMap>
             <div>
                 <MapForm onFormSubmit={handleFormSubmit}/>
+                <button onClick={fetchDirections} className="bg-blue-500 text-white px-4 py-2 mt-2">Show Directions
+                </button>
             </div>
         </div>
     )
